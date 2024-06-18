2c7c928640a9ade96026c168bd40fb4808c8451d
joram kamau <yllejorum@gmail.com>
2024-06-18 17:27:23 +0300

    second commit of the file simple interets with correction on spacing

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,7 +1,7 @@
-principle= 1000
-rate=0.05
-time=3
-interest=principle*rate*time
+principle = 1000
+rate = 0.05
+time = 3
+interest = principle * rate * time
 
 print ("The simple interets is ", (interest)
        )
