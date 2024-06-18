caec94bd1260e9bf1db60af42c5bc0cfeba88049
joram kamau <yllejorum@gmail.com>
2024-06-18 23:59:40 +0300

    finance calculator file created

---
+++ python_introduction/finance_calculator.py
@@ -0,0 +1,7 @@
+monthly_income = 5000
+monthly_expenses = 4000
+monthly_savings = int (monthly_income - monthly_expenses)
+interest_rate = 0.05
+#projected_savings = (monthly_savings, * 12, ( monthly_savings, *12, * interest_rate))
+print = (monthly_savings)
+#print = (projected_savings)
