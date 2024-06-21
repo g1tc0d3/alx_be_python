2ea46ab3d8c24fe81174d87937f8c0cdb98f5edc
joram kamau <yllejorum@gmail.com>
2024-06-21 23:30:57 +0300

    changed int function to original format

--- python_introduction/future_age_calculator.py
+++ python_introduction/future_age_calculator.py
@@ -1,4 +1,4 @@
-current_age = int (input ("How old are you?" ))
-age = (current_age) + 27
+current_age = (input ("How old are you?" ))
+age = int (current_age) + 27
 
 print ("In 2050 you will be ",age ,"years old")
