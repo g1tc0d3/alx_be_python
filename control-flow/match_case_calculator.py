fa2d6e2c938a75ebc1106710a1ae04359bb181d6
joram kamau <yllejorum@gmail.com>
2024-06-29 21:18:59 +0300

    changes on the match case file

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -1,17 +1,17 @@
 num1 = int(input ("Enter the first number: "))
 num2 = int(input ("Enter the second number: "))
 
-operation = str (input ("Choose the operation ( + , - , * , / ): "))
+operation = str(input ("Choose the operation ( + , - , * , / ): "))
 
 match operation :
     case "+":
-        print ( num1 + num2)
+        print ( "The result is ", num1 + num2)
     case "-":
-        print ( num1 - num2)
+        print ( "The result is ", num1 - num2)
     case "*":
-        print (num1 * num2)
+        print ( "The result is ", num1 * num2)
     case "/":
-        print (num1 / num2)
+        print ( "The result is ", num1 / num2)
     case _ :
         print ("Invalid operation")
 
