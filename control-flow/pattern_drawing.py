dffafb3aa1435cae16b14a9a3ab1bc32de1e1859
joram kamau <yllejorum@gmail.com>
2024-06-30 05:45:46 +0300

    last commit of the day

---
+++ control-flow/pattern_drawing.py
@@ -0,0 +1,9 @@
+size = int(input ( "Enter the size of the pattern: "))
+
+while 0 <= size:
+    for i in range (size):
+        #size = size - 1
+        #for j in range (size):
+        print ("*", end="")
+        print ()
+    
