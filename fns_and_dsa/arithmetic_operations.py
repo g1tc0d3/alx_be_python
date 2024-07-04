36afbde494c495f1828a92d0ba69fbe1d82a3df2
joram kamau <yllejorum@gmail.com>
2024-07-04 22:30:45 +0300

    added result to output

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -3,21 +3,22 @@ num2 = int(input("Enter the second number: "))
 operation = str(input("Enter the operation (add, subtract, multiply, divide): "))
 
 def perform_operation(num1, num2, operation):
+    # result = perform_operation(num1, num2, operation)
 
     match operation:
         case "add":
-            print (num1 + num2)
+            print ("Result:",num1 + num2)
         case "subtract":
-            print (num1 - num2)
+            print ("Result:",num1 - num2)
         case "multiply":
-            print (num1 * num2)
+            print ("Result:",num1 * num2)
         case "divide":
             if num1 ==0:
-                print ("Cannot recognize and display correctly")
+                print ("Result:","Cannot recognize and display correctly")
             elif num2 ==0:
-                print ("Cannot recognize and display correctly")
+                print ("Result:","Cannot recognize and display correctly")
             else:
-                print (num1 / num2)
+                print ("Result:",num1 / num2)
 perform_operation( num1,num2, operation)
 
 
