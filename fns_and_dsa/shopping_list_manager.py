862a261a2f81528d6a63f554b0bf332b8a967b23
joram kamau <yllejorum@gmail.com>
2024-07-05 23:45:35 +0300

    first commit for shopping list file

---
+++ fns_and_dsa/shopping_list_manager.py
@@ -0,0 +1,31 @@
+def display_menu():
+    print ("Shopping List Manager")
+    print ("1. Add Item")
+    print ("2. Remove Item")
+    print ("3. View List")
+    print ("4. Exit")
+
+def main():
+    shopping_list = []
+    while True:
+        display_menu()
+        choice = input ("Enter your choice: ")
+
+        if choice == "1":
+            item = ("Enter item to add: ")
+            shopping_list.append(item)
+            print(item, "added to the list")
+        elif choice == "2":
+            item = ("Enter item to remove: ")
+            if item in shopping_list:
+                shopping_list.remove(item)
+                print (item, "removed from list")
+            else:
+                print(item, "not found in the list")
+        elif choice == "3":
+            print (shopping_list)
+        elif choice == "4":
+            return()
+        else:
+            print ("Not a valid choice")
+main()
