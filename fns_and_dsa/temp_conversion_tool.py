d7f1532eb9a1c83e47ab412dab1cec4541b2e372
joram kamau <yllejorum@gmail.com>
2024-07-07 02:56:59 +0300

    corrected global variables

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -1,25 +1,15 @@
-def FAHRENHEIT_TO_CELSIUS_FACTOR():
-    factor = float(5/9)
-    # print (5/9)
-    return()
-def CELSIUS_TO_FAHRENHEIT_FACTOR():
-    # print (9/5)
-    return()
+FAHRENHEIT_TO_CELSIUS_FACTOR = 5/9
+CELSIUS_TO_FAHRENHEIT_FACTOR = 9/5
+    
 def convert_to_celsius ():
-    global FAHRENHEIT_TO_CELSIUS_FACTOR
     fahrenheit = float(input("Enter the temparature to convert: "))
     unit = input("Is this temparature in Celsius or Fareinheit? (C/F): ")
     match unit:
         case "F":
             conversion = ((fahrenheit - 32) / FAHRENHEIT_TO_CELSIUS_FACTOR)
+            print (f"{fahrenheit} is {conversion}")
         case "C":
             print("Temparature in Celsius")
         
 convert_to_celsius()
-CELSIUS_TO_FAHRENHEIT_FACTOR()
-FAHRENHEIT_TO_CELSIUS_FACTOR()
 
-    
-           
-FAHRENHEIT_TO_CELSIUS_FACTOR()
-CELSIUS_TO_FAHRENHEIT_FACTOR()
