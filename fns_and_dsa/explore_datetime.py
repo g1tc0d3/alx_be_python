dcd11b82b5aa88d602bcc0e987f09e4afa304e5f
joram kamau <yllejorum@gmail.com>
2024-07-06 18:05:16 +0300

    first commit for explore time file

---
+++ fns_and_dsa/explore_datetime.py
@@ -0,0 +1,8 @@
+import datetime
+
+def display_current_datetime(YYYY, MM, DD):
+    current_date = datetime.datetime(2024, 7, 6)
+    print (current_date)
+    return()
+
+display_current_datetime("YYYY", "MM", "DD")
