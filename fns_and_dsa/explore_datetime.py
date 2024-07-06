6df8707fbaaadcf161533d96372bad1bdb712d78
joram kamau <yllejorum@gmail.com>
2024-07-06 18:25:35 +0300

    corrected output for current date time

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -2,7 +2,7 @@ from datetime import datetime, timedelta, date
 
 def display_current_datetime(YYYY, MM, DD):
     current_date = datetime.now()
-    print (current_date)
+    print (f"Current date and time: {current_date}")
     return()
 
 display_current_datetime("YYYY", "MM", "DD")
