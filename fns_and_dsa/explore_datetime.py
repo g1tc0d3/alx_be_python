c8bf4098c4bbdf6999d81daa9fbb8d4cc24204ea
joram kamau <yllejorum@gmail.com>
2024-07-06 18:22:32 +0300

    included import for datetime

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -1,7 +1,7 @@
-import datetime
+from datetime import datetime, timedelta, date
 
 def display_current_datetime(YYYY, MM, DD):
-    current_date = datetime.datetime(2024, 7, 6)
+    current_date = datetime.now()
     print (current_date)
     return()
 
