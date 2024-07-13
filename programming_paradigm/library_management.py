af11d200c2fd3925226e49efca8fc6c6dfa2d07a
joram kamau <yllejorum@gmail.com>
2024-07-13 22:42:37 +0300

    fourht commit

--- programming_paradigm/library_management.py
+++ programming_paradigm/library_management.py
@@ -1,7 +1,13 @@
 class Book:
     def __init__ (self):
+        pass
+    def title (self, title):
         self.title = title
+        pass
+    def author (self, author):
         self.author = author
+        pass
+    def _is_checked_out (self, _is_checked_out):
         self._is_checked_out = _is_checked_out
         pass
     
