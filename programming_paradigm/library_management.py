1ba8f3cce744b963d70efd5ee1203d762e0c3114
joram kamau <yllejorum@gmail.com>
2024-07-13 22:45:01 +0300

    fifith commit with initializing

--- programming_paradigm/library_management.py
+++ programming_paradigm/library_management.py
@@ -1,18 +1,8 @@
 class Book:
-    def __init__ (self):
-        pass
-    def title (self, title):
-        self.title = title
-        pass
-    def author (self, author):
-        self.author = author
-        pass
-    def _is_checked_out (self, _is_checked_out):
-        self._is_checked_out = _is_checked_out
-        pass
+    def __init__(self):
     
 class Library:
-    def __init__ (self):
+    def __init__(self):
         self._books = _books
         self.add_book = Book.append()
         self.check_out_book = check_out_book
