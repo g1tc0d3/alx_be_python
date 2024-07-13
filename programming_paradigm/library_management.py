aa3ca6c22824a6ec5ef14a9315490b771b7879f3
joram kamau <yllejorum@gmail.com>
2024-07-13 23:32:57 +0300

    commit for library management file witht updated main file

--- programming_paradigm/library_management.py
+++ programming_paradigm/library_management.py
@@ -1,7 +1,13 @@
 class Book:
-    def __init__(self):
+    def __init__(self, title, author, _is_checked_out,):
+
+
         pass
     
 class Library:
-    def __init__(self):
+    def __init__(self, add_book, checkout_book, return_book, list_available_books):
+        self.add_book = Book.append()
+        self.checkout_book = checkout_book
+        self.return_book = return_book
+        self.list_available_books = list_available_books
         pass
