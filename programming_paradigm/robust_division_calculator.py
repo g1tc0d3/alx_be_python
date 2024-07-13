906442450e6d9733726beab9288738e359e85c0d
joram kamau <yllejorum@gmail.com>
2024-07-14 00:01:02 +0300

    modified file wth try error catching

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -1,7 +1,11 @@
 
 def safe_divide (numerator, denominator):
-    print (numerator/denominator)
-    if numerator or denominator == 0:
-        raise ZeroDivisionError ("Division by Zero not allowed")
 
-    return numerator/denominator
+    try:
+        if numerator or denominator == 0:
+            raise ZeroDivisionError ("Division by Zero not allowed")
+    except:
+        ValueError
+        print("Error! please enter numeric values only")
+
+        return numerator/denominator
