9dfacd3407a5a8b90abbcc88ddd1613d4dffcdc4
joram kamau <yllejorum@gmail.com>
2024-07-13 07:16:58 +0300

    created deposit and withdrawal

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -2,4 +2,14 @@ class BankAccount:
     def __init__(self, account_balance =0):
         self.account_balance = account_balance
 
+    def deposit (self, amount):
+        self.amount = input("Enter amount to deposit: ")
+        self.account_balance += amount
+        print (self.account_balance)
+
+    def withdraw (self, amount):
+        self.amount = input ("Enter amount to withdraw: ")
+        self.account_balance -= amount
+        print (self.account_balance)
+
         
