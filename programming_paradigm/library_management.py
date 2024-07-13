00d7bd8caf797fe6d401b640be2ba997f58ef8f0
joram kamau <yllejorum@gmail.com>
2024-07-13 22:48:51 +0300

    sixth commit o initializing file

--- programming_paradigm/library_management.py
+++ programming_paradigm/library_management.py
@@ -1,14 +1,7 @@
 class Book:
     def __init__(self):
+        pass
     
 class Library:
     def __init__(self):
-        self._books = _books
-        self.add_book = Book.append()
-        self.check_out_book = check_out_book
-        self.return_book = return_book
-        self.list_availbale_book = Book.list()
-
-
-
-
+        pass
