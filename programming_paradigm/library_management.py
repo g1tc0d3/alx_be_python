b7cd616e1b34ac1781e07f973a3a24fe97d54ffd
joram kamau <yllejorum@gmail.com>
2024-07-14 05:27:24 +0300

    commiting 3 files , test and simple calculator with library management

--- programming_paradigm/library_management.py
+++ programming_paradigm/library_management.py
@@ -1,5 +1,9 @@
 class Book:
     def __init__(self, title, author, _is_checked_out,):
+        self.title = title
+        self.author = author
+        self._is_checked_out = _is_checked_out
+
 
 
         pass
