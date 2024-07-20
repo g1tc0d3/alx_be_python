72848d965f2bc4a2c1b3128c0381289f826a10df
joram kamau <yllejorum@gmail.com>
2024-07-21 00:01:10 +0300

    commit changes to library file

--- oop/library_system.py
+++ oop/library_system.py
@@ -4,12 +4,12 @@ class Book:
         self.title = title
         self.author = author
 
-class EBook (Book):
+class EBook(Book):
     def __init__ (self, title, author, file_size):
         Book.__init__(self, title, author)
         self.file_size = file_size 
 
-class PrintBook (Book):
+class PrintBook(Book):
     def __init__ (self, title, author, page_count):
         Book.__init__(self, author, title)
         self.page_count = page_count
